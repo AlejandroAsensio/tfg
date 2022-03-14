8e3ecf6c848778bb596e9ffa5ebb6bf5dd884f72
AlejandroAsensio <alejandro.asensio.alvarez@gmail.com>
2022-03-14 10:05:36 +0100

    Clase Usuario con atributos

--- src/main/java/org/tfg/teafind/entities/Usuario.java
+++ src/main/java/org/tfg/teafind/entities/Usuario.java
@@ -1,15 +1,92 @@
 package org.tfg.teafind.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Usuario {
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
 	private String nombre;
-	
 	private String apellido1;
-	
 	private String apellido2;
-	
 	private String telefono;
+	
+	@Column(unique = true)
+	private String email;
+	
+	private String password;
+	private boolean admin;
+	//======================================================
+	public Usuario() {
+		
+	}
+	public Usuario(String nombre, String apellido1, String apellido2, String telefono, String email, String password,
+			boolean admin) {
+		super();
+		this.nombre = nombre;
+		this.apellido1 = apellido1;
+		this.apellido2 = apellido2;
+		this.telefono = telefono;
+		this.email = email;
+		this.password = password;
+		this.admin = admin;
+	}
+	//======================================================
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido1() {
+		return apellido1;
+	}
+	public void setApellido1(String apellido1) {
+		this.apellido1 = apellido1;
+	}
+	public String getApellido2() {
+		return apellido2;
+	}
+	public void setApellido2(String apellido2) {
+		this.apellido2 = apellido2;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public boolean isAdmin() {
+		return admin;
+	}
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
+	}
+	
+	
 }
