1db7d4e324576e4a79340cbb4236362c49e5bdbb
Chapa03 <chapaog@gmail.com>
2022-03-21 21:41:22 +0100

    Mejorada la vista Mis proyectos y comienzo de la vista Gestionar proyecto para los proyectos que lideras.

--- src/main/java/org/tfg/teafind/repository/ProyectoRepository.java
+++ src/main/java/org/tfg/teafind/repository/ProyectoRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.tfg.teafind.entities.Proyecto;
-import org.tfg.teafind.entities.Usuario;
 
 @Repository
 public interface ProyectoRepository extends JpaRepository<Proyecto, Long> {
