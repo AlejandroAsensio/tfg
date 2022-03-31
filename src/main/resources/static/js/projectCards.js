5a6705fc82d46fc7da4ad571772480c589ed3942
Chapa03 <chapaog@gmail.com>
2022-03-31 17:01:30 +0200

    Solucionados algunos problemas con el menú de login. Cambiada la vista de gestión de proyectos (sin terminar). Cambiada la fuente del proyecto.

--- src/main/resources/static/js/projectCards.js
+++ src/main/resources/static/js/projectCards.js
@@ -1,11 +1,9 @@
 function layoutProjectsGrid() {
-	console.log("function grid");
     $("div#cardsList").hide();
     $("div#cardsGrid").fadeIn(650);
 }
 
 function layoutProjectsList() {
-	console.log("function list")
     $("div#cardsGrid").hide();
     $("div#cardsList").fadeIn(650);
 }
