f676026fa5aeb33ccc9b01d4c031fea747532803
Chaparro Arteaga <cchaparro@indra.es>
2022-04-06 17:11:48 +0200

    Añadidos cambios a la vista de gestión de proyectos y botón que habilita o deshabilita los campos de información del proyecto para editar la información. Cambios pendientes en el modo de edición general

--- src/main/resources/static/js/edicionGestionProyectos.js
+++ src/main/resources/static/js/edicionGestionProyectos.js
@@ -1,11 +1,31 @@
+/**
+	Cambia la visibilidad de los botones generales de edición en la vista de gestión de proyecto
+ */
+function edicionGestionProyecto() {
+	$(".botonEdicionProyecto").toggleClass("hiddenInputProyecto");
+}
+
+/**
+	Habilita los campos editables en el formulario de Proyecto U en la vista de gestión de proyectos
+	y oculta los que mostraban la información
+ */
 function mostrarEdicionCamposGestionProyecto() {
     $(".hiddenInputProyecto").toggleClass("hiddenHinputProyecto").toggleClass("visibleHinputProyecto").toggle();
     $(".visibleInputProyecto").toggleClass("visibleHinputProyecto").toggleClass("hiddenHinputProyecto").toggle();
     $("div#jumboGeneralInfo").toggleClass("warningEdicionProyecto");
 }
 
+/**
+	Deshabilita los campos editables en el formulario de Proyecto U en la vista de gestión de proyectos
+	y habilita los que muestran la información
+ */
 function cancelarEdicionCamposGestionProyecto() {
     $(".hiddenInputProyecto").toggleClass("hiddenHinputProyecto").toggleClass("visibleHinputProyecto").toggle();
     $(".visibleInputProyecto").toggleClass("visibleHinputProyecto").toggleClass("hiddenHinputProyecto").toggle();
     $("div#jumboGeneralInfo").toggleClass("warningEdicionProyecto");
+}
+
+
+function mostrarEdicionEquipoGestionProyecto() {
+	
 }
