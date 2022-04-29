25871c1b0ccb2020e84b194dd809ed8ccf4c15ef
Chaparro Arteaga <cchaparro@indra.es>
2022-04-29 11:47:18 +0200

    Reescrito y rediseñado el botón de la navbar 'CrearProyecto'. Ahora Está hecho sólo en html y CSS, por lo que no da problemas de queue de jQuery en animaciones de apertura y cierre ni se acumulan las animaciones. Funciona perfectamente

--- src/main/resources/static/js/efectosJQ.js
+++ src/main/resources/static/js/efectosJQ.js
@@ -1,14 +1,3 @@
 $(document).ready(function() {
-	if (window.innerWidth >= 540) {
-		$(".textoBotonProyectos").hide();
-	}
 	
-	$("li.botonProyectos").hover(
-		function() {
-			$(".textoBotonProyectos").show(400);
-		},
-		function() {
-			$(".textoBotonProyectos").hide(400);
-		}
-	)
 })
