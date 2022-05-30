92f2a0f7d174132f0121d1264432c4e4bfee04fb
AlejandroAsensio <alejandro.asensio.alvarez@gmail.com>
2022-05-30 18:54:46 +0200

    Añadir puesto ya no funciona si no tiene nombre/descripcion/habilidades

--- src/main/resources/static/js/crearPuestos.js
+++ src/main/resources/static/js/crearPuestos.js
@@ -1,9 +1,34 @@
 function crearMasPuestos(seguir){
+	var nombre = document.getElementById("recipient-name").value;
+	var desc = document.getElementById("message-text").value;
+	var ok = true;
+	var okH = false;
+	
+	
+	if ($('input[name^=idsHabilidadesRequire]:checked').length <= 0) {
+       
+    }else{
+        okH = true;
+    }
+	
+	if(nombre == null || nombre == "" || desc == null || desc == ""){
+		ok = false;
+	
+		
+	}
+	
+	if(ok && okH){
 	if(seguir){
 		puestoForm.destino.value=seguir;
 	}
 	else if(!seguir){
 		puestoForm.destino.value=seguir;
 	}
-	puestoForm.submit();
+	
+	puestoForm.submit();	
+	}
+	else{
+		alert("Campos vacios");
+	}
+	
 }
