03e633fc99b911b957dd2006c1f6ee8a82622e87
Chapa03 <chapaog@gmail.com>
2022-06-03 00:30:14 +0200

    El toggle ya funciona, pero con los finalizados mostrándose por defecto

--- src/main/resources/static/js/efectos.js
+++ src/main/resources/static/js/efectos.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
     $("div#finalizadosSwitch").on("change", function() {
         $(".proyectoFinalizado").each(function (i){
-            console.log("Hiden nº " + i)
             $(this).prop("hidden", function(i, v) {
-                console.log("Toggle -" + v)
                 return !v;
             });
         })
