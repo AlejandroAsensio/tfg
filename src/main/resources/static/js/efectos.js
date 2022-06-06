4779ca76613601acfcc0f27a2a0737fbd8a8ddd4
Chaparro <cchaparro@indra.es>
2022-06-06 17:24:40 +0200

    Cambios para solucionar errores en el Switch

--- src/main/resources/static/js/efectos.js
+++ src/main/resources/static/js/efectos.js
@@ -1,9 +1,4 @@
 $(document).ready(function() {
-    $('.grid').masonry({
-        // options
-        itemSelector: '.grid-item',
-        columnWidth: 200
-      });
     $(".proyectoFinalizado").prop("hidden", true);
     $("div#finalizadosSwitch").on("change", function() {
         $(".proyectoFinalizado").each(function (i){
